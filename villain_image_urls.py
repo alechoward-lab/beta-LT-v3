575ea9208875532b21810c24460bfdc9a954fde6
alechoward-lab <alechoward41@gmail.com>
2025-05-29 22:44:54 -0400

    Add files via upload

--- villain_image_urls.py
+++ villain_image_urls.py
@@ -4,4 +4,8 @@ villain_image_urls = {
     "Rhino": "https://i.imgur.com/g51Eseo.jpeg",
     "Klaw": "https://i.imgur.com/xT5uItU.jpeg",
     "Ultron": "https://i.imgur.com/RpXFGVK.jpeg",
+    "Risky Business": "https://i.imgur.com/RjjNSrR.jpeg",
+    "Mutagen Formula": "https://i.imgur.com/E9LJfJK.jpeg",
+    "Wrecking Crew": "https://i.imgur.com/odJwudi.jpeg",
+    
 }
