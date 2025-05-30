4f7f7b4bdb7fc3f87e5f761412688436795a5e34
alechoward-lab <alechoward41@gmail.com>
2025-05-29 21:26:25 -0400

    Add files via upload

---
+++ villain_image_urls.py
@@ -0,0 +1,7 @@
+# Villain image urls
+
+hero_image_urls = {
+    "Rhino": "https://i.imgur.com/g51Eseo.jpeg",
+    "Klaw": "https://i.imgur.com/xT5uItU.jpeg",
+    "Ultron": "https://i.imgur.com/RpXFGVK.jpeg",
+}
--- villain_weights.py
+++ villain_weights.py
@@ -1,6 +1,6 @@
 # villain_weights.py
 villain_weights = {
     "Rhino": [4, 2, 2, 3, 5, 1, 3, 1, 0, 0, 0, 0, 2, 1, 2],
+    "Klaw": [3, 1, 2, 2, 3, 3, 4, 4, 0, 0, 0, 1, 1, 0, 1],
     "Ultron": [2, 2, 1, 3, 2, 4, 3, 5, 1, 0, 0, 1, 2, 0, 2],
-    # Add more villains...
 }
