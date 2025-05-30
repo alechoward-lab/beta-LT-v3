290aeae2f0e07238ceed09fa42dc2a70bbf98a41
alechoward-lab <alechoward41@gmail.com>
2025-05-29 21:27:25 -0400

    Add files via upload

--- villain_image_urls.py
+++ villain_image_urls.py
@@ -1,6 +1,6 @@
 # Villain image urls
 
-hero_image_urls = {
+villain_image_urls = {
     "Rhino": "https://i.imgur.com/g51Eseo.jpeg",
     "Klaw": "https://i.imgur.com/xT5uItU.jpeg",
     "Ultron": "https://i.imgur.com/RpXFGVK.jpeg",
