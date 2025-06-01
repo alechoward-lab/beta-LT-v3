f5b22e07bb4009a91c8a76d8a83ac7ccdcc341be
alechoward-lab <alechoward41@gmail.com>
2025-06-01 13:40:46 -0400

    Update villain_weights.py

--- villain_weights.py
+++ villain_weights.py
@@ -1,9 +1,16 @@
-# villain_weights.py
-villain_weights = {
-    "Rhino": [4, 2, 2, 3, 5, 1, 3, 1, 0, 0, 0, 0, 2, 1, 2],
-    "Klaw": [3, 1, 2, 2, 3, 3, 4, 4, 0, 0, 0, 1, 1, 0, 1],
-    "Ultron": [2, 2, 1, 3, 2, 4, 3, 5, 1, 0, 0, 1, 2, 0, 2],
-    "Risky Business": [2, 2, 1, 3, 2, 4, 3, 5, 1, 0, 0, 1, 2, 0, 2],
-    "Mutagen Formula": [2, 2, 1, 3, 2, 4, 3, 5, 1, 0, 0, 1, 2, 0, 2],
-    "Wrecking Crew": [2, 2, 1, 3, 2, 4, 3, 5, 1, 0, 0, 1, 2, 0, 2],
+# Villain image urls
+
+villain_image_urls = {
+    "Rhino": "https://i.imgur.com/g51Eseo.jpeg",
+    "Klaw": "https://i.imgur.com/xT5uItU.jpeg",
+    "Ultron": "https://i.imgur.com/RpXFGVK.jpeg",
+    "Risky Business": "https://i.imgur.com/RjjNSrR.jpeg",
+    "Mutagen Formula": "https://i.imgur.com/E9LJfJK.jpeg",
+    "Wrecking Crew": "https://i.imgur.com/odJwudi.jpeg",
+## Rise of Red Skull
+    "Crossbones": "https://i.imgur.com/Yh1nlna.jpeg",
+    "Absorbing Man": "https://i.imgur.com/nefdKCJ.jpeg",
+    "Taskmaster": "https://i.imgur.com/Q8yTJjF.jpeg",
+    "Zola": "https://i.imgur.com/8Lw7gFK.jpeg",
+    "Red Skull": "https://i.imgur.com/si4YArG.jpeg",
 }
