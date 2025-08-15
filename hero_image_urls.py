a57fc4d6e2372c2dd498ce9cc7c0660515765281
alechoward-lab <alechoward41@gmail.com>
2025-08-15 17:30:25 -0400

    Update hero_image_urls.py

--- hero_image_urls.py
+++ hero_image_urls.py
@@ -64,4 +64,7 @@ hero_image_urls = {
     "Nick Fury": "images/heroes/57_Nick_Fury.jpg",
     "Silk": "images/heroes/58_Silk.jpg",
     "Shuri": "images/heroes/59_Shuri.jpg",
+    "Falcon": "images/heroes/60_Falcon.jpg",
+    "Shuri": "images/heroes/61_Winter_Soldier.jpg",
+
 }
