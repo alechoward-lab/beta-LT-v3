629063f3d5664c050fcd37c1d2827eb730a4ac20
alechoward-lab <alechoward41@gmail.com>
2025-12-11 15:16:42 -0500

    Update Captain Marvel's attributes in default_heroes

--- default_heroes.py
+++ default_heroes.py
@@ -1,7 +1,7 @@
 import numpy as np
                             #              e, t, cv,s, d, th,re,mi,c, s, br,lg,si,sc,mu
 default_heroes = {  
-        "Captain Marvel":       np.array([ 5, 3,-1, 3, 4, 2, 4, 1, 0, 3, 0, 0, 5, 1, 1]),
+        "Captain Marvel":       np.array([ 5, 4,-1, 3, 4, 2, 4, 1, 0, 3, 0, 0, 5, 1, 1]),
         "Iron Man":             np.array([ 4,-5, 4, 2, 5, 2, 0, 3, 0, 0, 1, 4,-5, 0, 5]),
         "Spider-Man Peter":     np.array([ 4, 0, 3, 5, 3,-2, 2, 0, 4, 1, 2, 0, 0, 0, 0]),
         "Black Panther":        np.array([ 3,-1, 4, 3, 3, 1, 1, 5, 0, 0, 0, 3,-1, 0, 1]),
